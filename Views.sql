fc8bcf39950c1d209e40ba7f99cd4c2d86f6e164
Ana Eliza <anaelizaperobelli@gmail.com>
2024-05-12 00:51:55 -0300

    Views EX 2

--- Views.sql
+++ Views.sql
@@ -1,4 +1,7 @@
 --CREATE OR REPLACE VIEW EMPLOYEES_VU
 --AS SELECT employee_id, first_name EMPLOYEE, department_id
---FROM employees                         1
+--FROM employees                         1                    
 
+                
+--SELECT employee, department_id
+--FROM employees_vu                      2

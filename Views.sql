e59ed443c71cc2780f39ed2d5cc61fc2dd113dc7
Ana Eliza <anaelizaperobelli@gmail.com>
2024-05-12 00:58:33 -0300

    Views EX 3

--- Views.sql
+++ Views.sql
@@ -5,3 +5,12 @@
                 
 --SELECT employee, department_id
 --FROM employees_vu                      2
+
+
+--CREATE OR REPLACE VIEW DEPT50
+--(EMPNO, EMPLOYEE, DEPTNO)
+--AS SELECT employee_id, last_name, department_id
+--FROM employees
+--WHERE department_id = 50
+--WITH CHECK OPTION CONSTRAINT DEPT50_CK  3
+

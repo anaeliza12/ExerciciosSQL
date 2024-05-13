3f0ee44efafc8feec226e881f0dbe6dc2438c164
Ana Eliza <anaelizaperobelli@gmail.com>
2024-05-12 22:18:05 -0300

    CRUD utilizando subconsultas

---
+++ CrudSubConsultas.sql
@@ -0,0 +1,21 @@
+--INSERT INTO DEPT (DEPT_ID, DNAME)
+--SELECT department_id,department_name
+--FROM departments
+ 
+ 
+--UPDATE DEPT 
+--SET dname = (SELECT DEPARTMENT_NAME
+--             FROM DEPARTMENTS
+--             WHERE DEPARTMENT_ID = 60)
+--WHERE dept_id = (SELECT department_id
+--                 FROM departments
+---                 WHERE department_name = 'IT');
+
+
+--DELETE FROM DEPT
+--WHERE DNAME IN (SELECT DEPARTMENT_NAME
+--                FROM DEPARTMENTS
+--                WHERE LOCATION_ID = 1700)
+                
+
+
